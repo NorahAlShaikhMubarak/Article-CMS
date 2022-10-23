9f3e95efd1749e459aac703ea1f1f6b0bff9a5f7
NorahAlShaikhMubarak <50755701+NorahAlShaikhMubarak@users.noreply.github.com>
2022-10-23 12:38:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,21 +5,25 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'articlestorage1234'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'z8Ytn3agItTaWpeoEKvGZH/8+HIbBS/eGafMb2G5oTKnGdtPcDd1M2Nw+HcA+/DE10xSL+KtK4W4+AStSlcj8A=='
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cmsarticleserver.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'CMS-Article'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Nourah98fplij'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    #For Local machine
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")\
+    #    or 'sqlite:///'+os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:test@localhost/cms'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "~rk8Q~vzp9D5woauxTvEB4oqhPWPwaxPplAJIcJZ"
+    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -30,7 +34,7 @@ class Config(object):
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "93fb7f7c-3052-41cb-bc5f-5d7f318fa863"
+    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
